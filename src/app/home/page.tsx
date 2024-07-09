61e55062c29ba19cec8bbd7dd80c730127985353
abhishekbadole1 <abhishekb@blockshards.in>
2024-07-09 18:40:29 +0530

    generate UI created

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,5 +1,33 @@
+import { Grand_Hotel } from "next/font/google";
 import React from "react";
+import { TiHeartFullOutline } from "react-icons/ti";
 
-export default function page() {
-  return <div>page</div>;
+const grand_hotel = Grand_Hotel({
+  weight: "400",
+  subsets: ["latin"],
+});
+
+export default function Page() {
+  return (
+    <div
+      className={`${grand_hotel.className} w-screen h-screen relative overflow-hidden`}
+    >
+      <img
+        src="/background/bg-img.png"
+        alt="Background"
+        className="absolute inset-0 w-full h-full object-cover"
+      />
+      <div className="flex flex-col items-center h-full">
+        <h5 className="text-white text-center text-5xl z-10 mt-20">
+          Pickup line <br /> generator
+        </h5>
+
+        <button className="m-auto text-3xl flex items-center justify-center gap-2 text-center font-normal py-3 px-7 bg-primary text-white border border-none rounded shadow-lg transform  transition-transform">
+          <TiHeartFullOutline size={18} />
+          Generate one for me
+          <TiHeartFullOutline size={18} />
+        </button>
+      </div>
+    </div>
+  );
 }
